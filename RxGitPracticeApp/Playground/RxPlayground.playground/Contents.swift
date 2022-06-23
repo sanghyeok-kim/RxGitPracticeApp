03ca2954890df1dbf7ed5dda270a766d6a82418d
sanghyeok-kim <slsnsi1212@gmail.com>
2022-06-23 23:03:24 +0900

    chore: playground 테스트

---
+++ RxGitPracticeApp/Playground/RxPlayground.playground/Contents.swift
@@ -0,0 +1,55 @@
+import Foundation
+import RxSwift
+
+
+
+/// subscribe(onNext:)
+
+//Observable<Int>.just(1)
+//    .subscribe(onNext: {
+//        print($0)
+//    })
+//
+//Observable<Int>.of(1, 2, 3, 4, 5)
+//    .subscribe(onNext: {
+//        print($0)
+//    })
+//
+//Observable<Int>.from([1, 2, 3, 4, 5])
+//    .subscribe(onNext: {
+//        print($0)
+//    })
+
+
+
+/// subscribe
+
+//Observable<Int>.just(1)
+//    .subscribe {
+//        print($0)
+//    }
+
+//Observable<Int>.of(1, 2, 3, 4, 5)
+//    .subscribe {
+//        print($0)
+//    }
+
+//Observable<Int>.from([1, 2, 3, 4, 5])
+//    .subscribe {
+//        print($0)
+//    }
+
+
+
+//Observable<Int>.of(1, 2, 3)
+//    .subscribe {
+//        print($0)
+////        print($0.element)
+////        if $0.element == nil {
+////            print("asd")
+////        }
+////        if let element = $0.element {
+////            print(element)
+////        }
+//    }
+
